8b86a98d2c0f84be479eb1344558c0f79504162d
adrianbornas <adrian.bornas@gmail.com>
2023-09-27 22:39:19 +0200

    Added mob squash when player hits jumping

--- mob.gd
+++ mob.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+# Emitted when the player jumped on the mob.
+signal squashed
+
 # Minimum speed of the mob in meters per second.
 @export var min_speed = 10
 # Maximum speed of the mob in meters per second.
@@ -27,4 +30,8 @@ func _physics_process(_delta):
 
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+
+func squash():
+	squashed.emit()
 	queue_free()
