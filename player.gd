31bd339563b7d186bf9ededf6a51e897051c648b
adrianbornas <adrian.bornas@gmail.com>
2023-10-02 22:20:51 +0200

    Added player collision. Added player death logic. Stop spawning mobs when player dies on main scene

--- player.gd
+++ player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+# Emitted when the player was hit by a mob.
+signal hit
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
@@ -65,3 +68,10 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_mob_detector_body_entered(_body):
+	die()
